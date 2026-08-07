4a9caadcc219626253c72f29ca7c83f9fec636cf
hugo ferey <ferey.hugo@gmail.com>
2026-08-07 18:37:58 +0200

    feat: ajout de storage.js (persistance) et getNextMonth (utils/dates)

---
+++ Compte/src/services/storage.js
@@ -0,0 +1,78 @@
+/**
+ * Enregistre la liste des règles, dans le localStorage, sous forme de chaîne JSON.
+ * @param {Array} rulesParm - le tableau des règles à stocker
+ */
+
+export function setRules(rulesParm) {
+    const rulesString = JSON.stringify(rulesParm);
+    localStorage.setItem("rules", rulesString);
+}
+
+/**
+ * Récupère la liste des règles depuis le localStorage.
+ * @returns {Array} - le tableau des règles, ou un tableau vide si rien n'est stocké
+ */
+
+export function getRules(){
+    const rulesString = localStorage.getItem("rules");
+
+    if (rulesString === null){
+        return []
+    }else{
+        return JSON.parse(rulesString);
+    }
+}
+
+/**
+ * Définit le solde initial du compte
+ * @param {number} balance - solde initial du compte
+ */
+
+export function setInitialBalance(balance){
+    const balanceString = JSON.stringify(balance);
+    localStorage.setItem("initialBalance", balanceString);
+}
+
+/***
+ * Récupère le solde initial du compte, depuis le localStorage.
+ * @returns {number} solde initial du compte
+ */
+
+export function getInitialBalance() {
+    const balanceString = localStorage.getItem("initialBalance");
+
+    if (balanceString === null){
+        return 0.00
+    }else{
+        return JSON.parse(balanceString);
+    }
+}
+
+/***
+ * Enregistre les opérations du mois, dans le localStorage, sous forme de chaîne JSON.
+ * @param {string} month - identifiant du mois au format "YYYY-MM" pour la liste des opérations
+ * @param {Array} operations - liste les opération du mois
+ */
+
+export function setOperations(month, operations) {
+    const key = `operations-${month}`;
+    const operationsString = JSON.stringify(operations);
+    localStorage.setItem(key, operationsString);
+}
+
+/***
+ * Récupère les opérations du mois, depuis le localStorage.
+ * @param {string} month - identifiant du mois au format "YYYY-MM" pour récupérer la liste
+ * @returns{Array} - le tableau des opérations, ou un tableau vide si rien n'est stocké
+ */
+
+export function getOperations(month) {
+    const key = `operations-${month}`;
+    const operationsString = localStorage.getItem(key);
+
+    if (operationsString === null){
+        return []
+    }else{
+        return JSON.parse(operationsString);
+    }
+}
