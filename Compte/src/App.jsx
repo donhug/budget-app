2c302f5ae8275b640cd55489ab76468c58532f21
hugo ferey <ferey.hugo@gmail.com>
2026-09-07 15:34:48 +0200

    feat: premier affichage des listes d'operation mensuelles et ponctuelles

--- Compte/src/App.jsx
+++ Compte/src/App.jsx
@@ -1,23 +1,46 @@
 import './App.css'
 import { generateMonths, getBalance } from './services/budget'
+import { getOperations } from './services/storage';
 import { getCurrentMonth } from './utils/dates'
 import { useEffect, useState } from 'react'
 
 function App() {
   const currentMonth = getCurrentMonth();
   const [balance, setBalance] = useState(null);
+  const [monthOperation, setMonthOperation] = useState([]);
 
   useEffect(() => { 
     generateMonths(); 
     setBalance(getBalance(currentMonth));
+    setMonthOperation(getOperations(currentMonth));
 
   },[currentMonth])
 
+  const monthlyOps = monthOperation.filter(op => op.origin === "rule");
+  const ponctualOps = monthOperation.filter(op => op.origin === "manual");
+
   return (
+
+    <section>
+      
+      <div>
+        <p>Mois  :  {currentMonth}</p>
+        <p>solde  :  {balance}€ </p>
+      </div>
+
     <div>
-      <p>Mois  :  {currentMonth}</p>
-      <p>solde  :  {balance}</p>
+      <p>Opération mensuelle : </p>
+      {monthlyOps.map((op) => 
+      <p key = {op.id}>{op.label} : {op.value}€</p>
+      )}
     </div>
+    <div>
+      <p>Opération ponctuelle : </p>
+      {ponctualOps.map((op) => 
+      <p key = {op.id}>{op.label} : {op.value}€</p>
+      )}
+    </div>
+    </section>
   )
 }
 
