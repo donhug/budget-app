6a89657d88b287af057b8dd1c8d1d0a20f6ea3f8
hugo ferey <ferey.hugo@gmail.com>
2026-09-07 11:56:38 +0200

    feat: premier affichage du mois courant et du solde

--- Compte/src/App.jsx
+++ Compte/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import { generateMonths, getBalance } from './services/budget'
+import { getCurrentMonth } from './utils/dates'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const currentMonth = getCurrentMonth();
+  const [balance, setBalance] = useState(null);
+
+  useEffect(() => { 
+    generateMonths(); 
+    setBalance(getBalance(currentMonth));
+
+  },[currentMonth])
 
   return (
-    <>
-    </>
+    <div>
+      <p>Mois  :  {currentMonth}</p>
+      <p>solde  :  {balance}</p>
+    </div>
   )
 }
 
