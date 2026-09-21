aa2c92d8175dbbee416eccdff23057f48b667e29
hugo ferey <ferey.hugo@gmail.com>
2026-09-21 22:22:03 +0200

    style: ajustements de la sidebar

--- Compte/src/components/Sidebar/Sidebar.jsx
+++ Compte/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,29 @@
 import style from "./Sidebar.module.css";
+import  LOGO  from "../../assets/LOGO.png";
 
 function Sidebar() {
-  return <aside className={style.sidebar}></aside>;
+  return (
+    <aside className={style.sidebar}>
+      <img src={LOGO} alt="Hugo Finances" className={style.logo} />
+      <nav className={style.nav}>
+        <a href="#" className={style.link}>
+          Dashboard
+        </a>
+        <a href="#" className={style.link}>
+          Modifications
+        </a>
+        <a href="#" className={style.link}>
+          En Cours
+        </a>
+        <a href="#" className={style.link}>
+          Epargne
+        </a>
+        <a href="#" className={style.link}>
+          Compte
+        </a>
+      </nav>
+    </aside>
+  );
 }
 
 export default Sidebar;
