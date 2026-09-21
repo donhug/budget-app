0e42a536c3fd74df036aa97ddd9d44e90f02527e
hugo ferey <ferey.hugo@gmail.com>
2026-09-21 22:05:33 +0200

    style: préparation de la mise en page pour la sidebar

--- Compte/src/App.jsx
+++ Compte/src/App.jsx
@@ -21,6 +21,7 @@ import { useEffect, useState } from "react";
 import OperationList from "./components/OperationList/OperationList";
 import OperationFormModal from "./components/OperationFormModal/OperationFormModal";
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   const currentMonth = getCurrentMonth();
@@ -98,63 +99,67 @@ function App() {
   const manualTotal = getMonthTotal(manualOps);
 
   return (
-    <>
-    <Header/>
-      <section className={styles.appshell}>
-        <div>
-          <MonthSummary
-            currentMonth={currentMonth}
-            balance={balance}
-            previousMonth={previousMonth}
-            carryOver={carryOver}
-            isFirstMonth={isFirstMonth}
-          />
-          <button
-            className={styles.addBtn}
-            onClick={() => setIsModalOpen(true)}
-          >
-            +AJOUTER
-          </button>
-          {isModalOpen && (
-            <OperationFormModal
-              onSubmit={handleSubmit}
-              onClose={() => setIsModalOpen(false)}
-            />
-          )}
-        </div>
-        <div className={styles.listGrid}>
+    <div className={styles.layoutWrapper}>
+      <Sidebar />
+      <div className={styles.mainColumn}>
+        <Header />
+
+        <section className={styles.appShell}>
           <div>
-            <OperationList
-              title="Opérations mensuelles : "
-              operations={monthlyOps}
-              total={monthlyTotal}
-              onDelete={(op) => setOperationToDelete(op)}
+            <MonthSummary
+              currentMonth={currentMonth}
+              balance={balance}
+              previousMonth={previousMonth}
+              carryOver={carryOver}
+              isFirstMonth={isFirstMonth}
             />
-            {operationToDelete && (
-              <DeleteConfirmation
-                label={operationToDelete.label}
-                onDeleteOperation={() => {
-                  handleDeleteOperation(operationToDelete.id);
-                  setOperationToDelete(null);
-                }}
-                onDeleteRule={handleDeleteRule}
-                onCancel={() => {
-                  setOperationToDelete(null);
-                }}
+            <button
+              className={styles.addBtn}
+              onClick={() => setIsModalOpen(true)}
+            >
+              +AJOUTER
+            </button>
+            {isModalOpen && (
+              <OperationFormModal
+                onSubmit={handleSubmit}
+                onClose={() => setIsModalOpen(false)}
               />
             )}
           </div>
-          <div>
-            <OperationList
-              title="Opérations ponctuelles : "
-              operations={manualOps}
-              total={manualTotal}
-              onDelete={(op) => handleDeleteOperation(op.id)}
-            />
+          <div className={styles.listsGrid}>
+            <div>
+              <OperationList
+                title="Opérations mensuelles : "
+                operations={monthlyOps}
+                total={monthlyTotal}
+                onDelete={(op) => setOperationToDelete(op)}
+              />
+              {operationToDelete && (
+                <DeleteConfirmation
+                  label={operationToDelete.label}
+                  onDeleteOperation={() => {
+                    handleDeleteOperation(operationToDelete.id);
+                    setOperationToDelete(null);
+                  }}
+                  onDeleteRule={handleDeleteRule}
+                  onCancel={() => {
+                    setOperationToDelete(null);
+                  }}
+                />
+              )}
+            </div>
+            <div>
+              <OperationList
+                title="Opérations ponctuelles : "
+                operations={manualOps}
+                total={manualTotal}
+                onDelete={(op) => handleDeleteOperation(op.id)}
+              />
+            </div>
           </div>
-        </div>
-      </section>
-    </>
+        </section>
+      </div>
+    </div>
   );
 }
 
---
+++ Compte/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,7 @@
+import style from "./Sidebar.module.css";
+
+function Sidebar() {
+  return <aside className={style.sidebar}></aside>;
+}
+
+export default Sidebar;
