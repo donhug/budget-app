2c7d78fefc6c60dfbe41109c5af0c5d99ac51293
hugo ferey <ferey.hugo@gmail.com>
2026-09-22 22:09:42 +0200

    feat: ajout d'une sidebar avec effet de glissement au survol

--- Compte/src/components/Sidebar/Sidebar.jsx
+++ Compte/src/components/Sidebar/Sidebar.jsx
@@ -1,26 +1,47 @@
+import { useState, useRef } from "react";
 import style from "./Sidebar.module.css";
-import  LOGO  from "../../assets/LOGO.png";
+import LOGO from "../../assets/LOGO.png";
+
+const links = ["Dashboard", "Modifications", "En Cours", "Epargne", "Compte"];
 
 function Sidebar() {
+  const [indicator, setIndicator] = useState(null);
+  const navRef = useRef(null);
+
+  function handleEnter(e) {
+    const navTop = navRef.current.getBoundingClientRect().top;
+    const linkRect = e.currentTarget.getBoundingClientRect();
+    setIndicator({
+      top: linkRect.top - navTop,
+      height: linkRect.height,
+    });
+  }
+
   return (
     <aside className={style.sidebar}>
       <img src={LOGO} alt="Hugo Finances" className={style.logo} />
-      <nav className={style.nav}>
-        <a href="#" className={style.link}>
-          Dashboard
-        </a>
-        <a href="#" className={style.link}>
-          Modifications
-        </a>
-        <a href="#" className={style.link}>
-          En Cours
-        </a>
-        <a href="#" className={style.link}>
-          Epargne
-        </a>
-        <a href="#" className={style.link}>
-          Compte
-        </a>
+
+      <nav
+        className={style.nav}
+        ref={navRef}
+        onMouseLeave={() => setIndicator(null)}
+      >
+        {indicator && (
+          <span
+            className={style.indicator}
+            style={{ top: indicator.top, height: indicator.height }}
+          />
+        )}
+        {links.map((label) => (
+          <a
+            key={label}
+            href="#"
+            className={style.link}
+            onMouseEnter={handleEnter}
+          >
+            {label}
+          </a>
+        ))}
       </nav>
     </aside>
   );
